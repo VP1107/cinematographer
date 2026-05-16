00a4d35249aa661bedcffe81548220443019a4e8
vatsal pandya <vatsalpandya2007@gmail.com>
2026-05-16 23:13:05 +0530

    Updated the routes forimages

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.jsx';
 
 // Global styles are imported inside App.jsx so that Vite's
 // CSS code-splitting works correctly with lazy routes.
